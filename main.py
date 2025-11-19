791d936259e7c57e92b24bbfe9756b3907c0e7d7
muztishini <111636144+muztishini@users.noreply.github.com>
2025-11-19 14:34:52 +0500

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def print_hi(name):
+import csv
+from tabulate import tabulate
+from collections import defaultdict
 
-    print(f'Hi, {name}!')
+
+
+def load_table(table):
+    with open(table, mode='r') as file:
+        csv_reader = csv.reader(file)
+        lst = []
+        for row in csv_reader:
+            lst.append(row)
+    return lst
+
+
+def func1(data):
+    lst = []
+    for item in data:
+        lst.append((item[1], item[3]))
+    return lst
+
+
+def func2(data):
+    data = data[1:]
+    positions_dict = defaultdict(list)
+    for position, performance in data:
+        positions_dict[position].append(float(performance))
+    result = [(pos, round(sum(scores) / len(scores), 1)) for pos, scores in positions_dict.items()]
+    sorted_lst = sorted(result, key=lambda x: x[1], reverse=True)
+    return sorted_lst
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    data1 = load_table('employees1.csv')
+    data2 = load_table('employees2.csv')
+    extracted_data1 = func1(data1)
+    extracted_data2 = func1(data2)
+    general_list = extracted_data1 + extracted_data2[1:]
+    final_data = func2(general_list)
+    headers = ["position", "performance"]
+    print(tabulate(final_data, headers=headers, tablefmt='fancy_grid'))
